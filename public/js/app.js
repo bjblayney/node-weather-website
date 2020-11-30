c130a7ddf4418ab6692ff2427489c6cc4a99c91b
bjblayney <bj.blayney@gmail.com>
2020-11-29 22:55:04 -0500

    remove console.lg call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("JS file from public");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
